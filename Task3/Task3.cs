a08ba5e991470cb098a81f0df15d4154617c5607
Siluet3656 <Mingaleev02@mail.ru>
2026-04-29 00:24:54 +0400

    Task 3

--- Task3/Task3.cs
+++ Task3/Task3.cs
@@ -1,4 +1,7 @@
-﻿namespace PerformanceLabTest.Task2;
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace PerformanceLabTest.Task3;
 
 public static class Task2
 {
@@ -6,7 +9,140 @@ public static class Task2
     {
         using var writer = new StreamWriter(Console.OpenStandardOutput());
 
+        if (args.Length != 3)
+        {
+            writer.WriteLine("Usage: PerformanceLabTest.exe valuesPath testsPath reportPath");
+            return;
+        }
+
+        string valuesPath = args[0];
+        string testsPath = args[1];
+        string reportPath = args[2];
+
+        Dictionary<int, JsonNode?> lookup;
+        JsonNode? structureToProcess;
+        JsonNode? testsRoot;
+
+        try
+        {
+            lookup = new Dictionary<int, JsonNode?>();
+            JsonNode? valuesRoot = JsonNode.Parse(File.ReadAllText(valuesPath));
+
+            JsonArray? valuesArray = null;
+            if (valuesRoot is JsonArray array)
+            {
+                valuesArray = array;
+            }
+            else if (valuesRoot is JsonObject obj && obj.TryGetPropertyValue("values", out JsonNode? valsNode) &&
+                     valsNode is JsonArray valsArray)
+            {
+                valuesArray = valsArray;
+            }
+
+            if (valuesArray != null)
+            {
+                foreach (JsonNode? item in valuesArray)
+                {
+                    if (item is JsonObject objItem &&
+                        objItem.TryGetPropertyValue("id", out JsonNode? idNode) && idNode != null &&
+                        objItem.TryGetPropertyValue("value", out JsonNode? valueNode))
+                    {
+                        int id = idNode.GetValue<int>();
+                        lookup[id] = valueNode?.DeepClone();
+                    }
+                }
+            }
+        }
+        catch (Exception exception)
+        {
+            Console.Error.WriteLine($"Error reading ellipse file: {exception.Message}");
+            return;
+        }
+
+        try
+        {
+            testsRoot = JsonNode.Parse(File.ReadAllText(testsPath));
+
+            structureToProcess = testsRoot;
+            if (testsRoot is JsonObject rootObj && rootObj.TryGetPropertyValue("tests", out JsonNode? testsNode) &&
+                testsNode is JsonArray)
+            {
+                structureToProcess = testsNode;
+            }
+            else if (testsRoot is JsonArray)
+            {
+                structureToProcess = testsRoot;
+            }
+        }
+        catch (Exception exception)
+        {
+            Console.Error.WriteLine($"Error reading ellipse file: {exception.Message}");
+            return;
+        }
+
+        if (structureToProcess != null && lookup.Count > 0)
+        {
+            FillValues(structureToProcess, lookup);
+        }
+
+        try
+        {
+            JsonNode? reportRoot = testsRoot;
+            if (testsRoot is JsonObject rootObjWithTests && rootObjWithTests.TryGetPropertyValue("tests", out _))
+            {
+                reportRoot = testsRoot;
+            }
+            else if (structureToProcess is JsonArray)
+            {
+                reportRoot = structureToProcess;
+            }
+
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            };
+
+            string reportJson = reportRoot?.ToJsonString(options) ?? "{}";
+            File.WriteAllText(reportPath, reportJson);
+
+            Console.WriteLine("Report saved in: " + reportPath);
+        }
+        catch (Exception exception)
+        {
+            Console.Error.WriteLine($"Error reading ellipse file: {exception.Message}");
+            return;
+        }
 
         writer.Flush();
+    }
+
+    static void FillValues(JsonNode? node, Dictionary<int, JsonNode?> lookup)
+    {
+        if (node == null) return;
+
+        if (node is JsonObject obj)
+        {
+            if (obj.TryGetPropertyValue("id", out JsonNode? idNode) &&
+                idNode != null &&
+                idNode.GetValue<int>() is int id &&
+                lookup.TryGetValue(id, out JsonNode? newValue))
+            {
+                obj["value"] = newValue?.DeepClone();
+            }
+
+            foreach (var property in obj)
+            {
+                if (property.Value is JsonObject || property.Value is JsonArray)
+                    FillValues(property.Value, lookup);
+            }
+        }
+        else if (node is JsonArray arr)
+        {
+            foreach (JsonNode? item in arr)
+            {
+                FillValues(item, lookup);
+            }
+        }
     }
 }
