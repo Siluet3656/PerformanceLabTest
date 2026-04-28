b4ff590cdb832186a55d7c2cdda2dfe26278a2ab
Siluet3656 <Mingaleev02@mail.ru>
2026-04-28 14:11:51 +0400

    task1 refactored

--- Task1/Task1.cs
+++ Task1/Task1.cs
@@ -5,94 +5,73 @@ public static class Task1
     static void Main(string[] args)
     {
         using var writer = new StreamWriter(Console.OpenStandardOutput());
-        
-        if (args.Length == 4)
+
+        if (args.Length != 4)
         {
-            int n1 = int.Parse(args[0]);
-            int m1 = int.Parse(args[1]);
-            int n2 = int.Parse(args[2]);
-            int m2 = int.Parse(args[3]);
-
-            if (n1 > 0 && m1 > 0 && n2 > 0 && m2 > 0)
-            {
-                int[] array1 = new int[n1];
-                int[] array2 = new int[n2];
-
-                for (int i = 0; i < n1; i++)
-                {
-                    array1[i] = i + 1;
-                }
-
-                for (int i = 0; i < n2; i++)
-                {
-                    array2[i] = i + 1;
-                }
-
-                int c = 0;
-                bool isEnd = false;
-                while (isEnd == false)
-                {
-                    for (int i = 0; i < m1; i++)
-                    {
-                        int cycleIndex = CalculateCycleIndex(c, n1);
-
-                        if (i == 0)
-                        {
-                           writer.Write(array1[cycleIndex]);
-                        }
-                        
-                        if (i == m1 - 1)
-                        {
-                            if (array1[cycleIndex] == 1)
-                            {
-                                isEnd = true;
-                            }
-                            break;
-                        }
-                        
-                        c++;
-                    }
-                }
-                
-                c = 0;
-                isEnd = false;
-                while (isEnd == false)
-                {
-                    for (int i = 0; i < m2; i++)
-                    {
-                        int cycleIndex = CalculateCycleIndex(c, n2);
-
-                        if (i == 0)
-                        {
-                            writer.Write(array2[cycleIndex]);
-                        }
-                        
-                        if (i == m2 - 1)
-                        {
-                            if (array2[cycleIndex] == 1)
-                            {
-                                isEnd = true;
-                            }
-                            break;
-                        }
-                        
-                        c++;
-                    }
-                }
-            }
-            else
-            {
-                writer.WriteLine("All arguments MUST be positive..");
-                writer.WriteLine("Usage: PerformanceLabTest.exe n1 m1 n2 m2");
-            }
-        }
-        else
-        {
-            writer.WriteLine("No arguments provided..");
+            writer.WriteLine("Wrong arguments provided..");
             writer.WriteLine("Usage: PerformanceLabTest.exe n1 m1 n2 m2");
+            writer.Flush();
+            return;
         }
         
+        if (!int.TryParse(args[0], out int n1) || !int.TryParse(args[1], out int m1) ||
+            !int.TryParse(args[2], out int n2) || !int.TryParse(args[3], out int m2))
+        {
+            writer.WriteLine("All arguments must be valid integers..");
+            writer.Flush();
+            return;
+        }
+
+        if (n1 > 0 && m1 > 0 && n2 > 0 && m2 > 0)
+        {
+            writer.WriteLine("All arguments MUST be positive..");
+            writer.WriteLine("Usage: PerformanceLabTest.exe n1 m1 n2 m2");
+            writer.Flush();
+            return;
+        }
+
+        Task<string> task1 = Task.Run(() => CalculatePath(n1, m1));
+        Task<string> task2 = Task.Run(() => CalculatePath(n2, m2));
+
+        Task.WaitAll(task1, task2);
+        
+        writer.Write(task1.Result + task2.Result);
+
         writer.Flush();
+    }
+
+    private static string CalculatePath(int n, int m)
+    {
+        string result = "";
+        
+        int c = 0;
+        bool isEnd = false;
+        while (isEnd == false)
+        {
+            for (int i = 0; i < m; i++)
+            {
+                int cycleIndex = CalculateCycleIndex(c, n);
+
+                if (i == 0)
+                {
+                    result += (cycleIndex + 1).ToString();
+                }
+
+                if (i == m - 1)
+                {
+                    if (cycleIndex + 1 == 1)
+                    {
+                        isEnd = true;
+                    }
+
+                    break;
+                }
+
+                c++;
+            }
+        }
+
+        return result;
     }
 
     private static int CalculateCycleIndex(int index, int size)
