94409f947d03452902b7ceafdcc3893c756f2ff3
Siluet3656 <Mingaleev02@mail.ru>
2026-04-28 20:43:22 +0400

    Project settings update

---
+++ Task3/Task3.cs
@@ -0,0 +1,12 @@
+﻿namespace PerformanceLabTest.Task2;
+
+public static class Task2
+{
+    static void Main(string[] args)
+    {
+        using var writer = new StreamWriter(Console.OpenStandardOutput());
+
+
+        writer.Flush();
+    }
+}
